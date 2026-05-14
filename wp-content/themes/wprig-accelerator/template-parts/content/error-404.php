8319a6235d590442f3a19004a4352a8b202bb321
“Simon <Simon.tucker9@icloud.com>
2026-05-14 14:45:10 +1200

    Revert 404 page search form changes to original state

--- wp-content/themes/wprig-accelerator/template-parts/content/error-404.php
+++ wp-content/themes/wprig-accelerator/template-parts/content/error-404.php
@@ -16,14 +16,9 @@ namespace WP_Rig\WP_Rig;
 			<?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'wp-rig' ); ?>
 		</p>
 
-		<div class="error-404-search">
-			<?php
-			wp_rig()->print_styles( 'wp-rig-content' );
-			get_search_form();
-			?>
-		</div><!-- .error-404-search -->
-
 		<?php
+		get_search_form();
+
 		wp_rig()->print_styles( 'wp-rig-widgets' );
 		the_widget( 'WP_Widget_Recent_Posts' );
 		?>
