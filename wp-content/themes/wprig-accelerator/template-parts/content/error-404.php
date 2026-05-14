6e31483e44ce2cb319a637282b66a7747e5046cf
“Simon <Simon.tucker9@icloud.com>
2026-05-14 15:05:23 +1200

    updated 404 page and customcss for search button

--- wp-content/themes/wprig-accelerator/template-parts/content/error-404.php
+++ wp-content/themes/wprig-accelerator/template-parts/content/error-404.php
@@ -16,36 +16,16 @@ namespace WP_Rig\WP_Rig;
 			<?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'wp-rig' ); ?>
 		</p>
 
-		<?php
-		get_search_form();
+		<div class="error-404-search">
+			<?php
+			get_search_form();
 
 		wp_rig()->print_styles( 'wp-rig-widgets' );
 		the_widget( 'WP_Widget_Recent_Posts' );
 		?>
+</div>
+		
 
-		<div class="widget widget_categories">
-			<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'wp-rig' ); ?></h2>
-			<ul>
-			<?php
-			wp_list_categories(
-				array(
-					'orderby'    => 'count',
-					'order'      => 'DESC',
-					'show_count' => 1,
-					'title_li'   => '',
-					'number'     => 10,
-				)
-			);
-			?>
-			</ul>
-		</div><!-- .widget -->
-
-		<?php
-		/* translators: %1$s: smiley */
-		$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'wp-rig' ), convert_smilies( ':)' ) ) . '</p>';
-		the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-		the_widget( 'WP_Widget_Tag_Cloud' );
-		?>
+		
 	</div><!-- .page-content -->
 </section><!-- .error -->
