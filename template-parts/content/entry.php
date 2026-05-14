31f41baf8e24782ed9e3f6c84f220da3d2ffeaac
Simon <simon.tucker9@icloud.com>
2026-05-14 22:23:32 +1200

    commented out the reply section of posts

--- template-parts/content/entry.php
+++ template-parts/content/entry.php
@@ -36,7 +36,7 @@ if ( is_singular( get_post_type() ) ) {
 	}
 
 	// Show comments only when the post type supports it and when comments are open or at least one comment exists.
-	if ( post_type_supports( get_post_type(), 'comments' ) && ( comments_open() || get_comments_number() ) ) {
-		comments_template();
-	}
+	// if ( post_type_supports( get_post_type(), 'comments' ) && ( comments_open() || get_comments_number() ) ) {
+	// 	comments_template();
+	// }
 }
