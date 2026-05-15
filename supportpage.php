2b468696dbf8bf8aae70d75a689e06318dfc4127
stuck83 <simon.tucker9@icloud.com>
2026-05-15 14:36:05 +1200

    added a contactpage and supportpage

---
+++ supportpage.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template Name: Support Page
+ *
+ * @package wprig
+ */
+
+// Remove has-sidebar body class on this template
+add_filter( 'body_class', function( $classes ) {
+    return array_diff( $classes, array( 'has-sidebar' ) );
+} );
+
+get_header();
+?>
+<main id="primary" class="site-main">
+<?php while ( have_posts() ) : the_post(); ?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class( 'support-page-article' ); ?>>
+        <div class="entry-content">
+            <?php the_content(); ?>
+        </div>
+    </article>
+<?php endwhile; ?>
+</main>
+<?php get_footer();
